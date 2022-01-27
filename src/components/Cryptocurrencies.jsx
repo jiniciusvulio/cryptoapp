3f788f37771111f66c06fd68c9251e4c292e5d3d
jinicius vulio <jiniciusvulio@gmail.com>
2022-01-27 14:57:55 -0300

    included all funcionalities except exchanges

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Loader from './Loader';
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const {data: cryptosList, isFetching} = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState([]);
+  const [cryptos, setCryptos] = useState();
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(()=>{
@@ -18,7 +19,7 @@ const Cryptocurrencies = ({ simplified }) => {
 
   }, [cryptosList, searchTerm]);
 
-  if(isFetching) return 'Loading...';
+  if(isFetching) return <Loader />;
 
   console.log(cryptos);
 
@@ -26,13 +27,13 @@ const Cryptocurrencies = ({ simplified }) => {
     <>
       {!simplified && (
       <div className="search-crypto">
-        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value.toLowerCase())} />
       </div>
       )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+            <Link to={`/crypto/${currency.uuid}`} key={currency.uuid}>
               <Card 
                 title={`${currency.rank}. ${currency.name}`}
                 // eslint-disable-next-line jsx-a11y/alt-text
